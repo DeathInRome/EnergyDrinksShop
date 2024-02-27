ac927dfcb91a98f5d73cbd323264377696603cf3
baa-1997 <politechkol@gmail.com>
2024-02-27 21:48:41 +0300

    Добавьте файлы проекта.

---
+++ EnergyDrinksShop/Program.cs
@@ -0,0 +1,13 @@
+public class Program
+{
+    internal static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello World!");
+
+        app.Run();
+    }
+
+}
